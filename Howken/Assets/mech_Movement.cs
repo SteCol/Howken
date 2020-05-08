7561c017df80f494b4525236d8c984246d2b49e6
Stef Colruyt <stef.colruyt@altereyes.com>
2020-05-08 16:42:00 +0200

    Added mech and basic movement

---
+++ Howken/Assets/mech_Movement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mech_Movement : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.Translate(new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime, 0, Input.GetAxis("Vertical")*Time.deltaTime));
+    }
+}
